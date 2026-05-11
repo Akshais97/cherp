3b4c7bc61734afb6b4ab39d8c02448cddaad8eb9
Akshais97 <akshaiofficial97@gmail.com>
2026-05-11 19:10:14 +0530

    auth added for first user, First Slice of work done

--- backend/dist/auth/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { RequestUser } from '../common/types/request-user.type';
-export declare class AuthController {
-    logout(user: RequestUser): {
-        user_id: string;
-        message: string;
-    };
-}
--- backend/dist/src/common/enums/user-role.enum.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum UserRole {
-    SuperAdmin = "super_admin",
-    ProjectManager = "project_manager",
-    TeamMember = "team_member",
-    Client = "client"
-}
